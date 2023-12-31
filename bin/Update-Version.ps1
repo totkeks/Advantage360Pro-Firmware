d05512bce6380093569528bf90c248be94c3fa49
Norman Dankert <norman.dankert@outlook.com>
2024-01-01 00:14:23 +0100

    Add PowerShell variant of the version macro update script

---
+++ bin/Update-Version.ps1
@@ -0,0 +1,33 @@
+$TIMESTAMP ??= (Get-Date -Format "yyyyMMddHHmm")
+$COMMIT ??= (& git rev-parse --short HEAD)
+$BRANCH ??= (& git rev-parse --abbrev-ref HEAD)
+
+function ConvertTo-ZMKKeyBehaviour (
+	[string]$string
+) {
+	$result = foreach ($char in $string.ToCharArray()) {
+		switch -regex ($char) {
+			"[A-Za-z]" { "<&kp $([char]::ToUpper($char))>" }
+			"[0-9]" { "<&kp N$char>" }
+			"\." { "<&kp DOT>" }
+			"-" { "<&kp MINUS>" }
+			"`n" { "<&kp RET>" }
+		}
+	}
+	$result -join ", "
+}
+
+$version = "$TIMESTAMP-$BRANCH-$COMMIT`n"
+$versionBindings = ConvertTo-ZMKKeyBehaviour $version
+"Version $version$versionBindings"
+
+$macro = @"
+macro_ver: macro_ver {
+	compatible = "zmk,behavior-macro";
+	label = "macro_ver";
+	#binding-cells = <0>;
+	bindings = $versionBindings;
+};
+"@
+
+Set-Content -Path "config/version.dtsi" -Value $macro -Force
