beec30aca0eeea77dea71472a1c50e538285113e
Norman Dankert <norman.dankert@outlook.com>
2024-01-01 12:08:30 +0100

    Refactor build scripts

--- bin/build.sh
+++ bin/build.sh
@@ -8,9 +8,8 @@ PWD=$(pwd)
 
 west build -s zmk/app -d build/left -b adv360_left -- -DZMK_CONFIG="${PWD}/config"
 grep -vE '(^#|^$)' build/left/zephyr/.config
+cp build/left/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}-${COMMIT}-left.uf2"
 
 west build -s zmk/app -d build/right -b adv360_right -- -DZMK_CONFIG="${PWD}/config"
 grep -vE '(^#|^$)' build/right/zephyr/.config
-
-cp build/left/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}-${COMMIT}-left.uf2"
 cp build/right/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}-${COMMIT}-right.uf2"
